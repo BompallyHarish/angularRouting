4c0ce1e3b3a6447010781fdb067e5d595753fee9
Harish Bompally <harishb@zenwork.com>
2024-06-03 18:40:47 +0530

    ngDestroy Changes.

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,24 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   constructor(private route: ActivatedRoute) { }
   userId: { id: number, name: string }
+  paramsSubscription: Subscription
   ngOnInit(): void {
     this.userId = {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     }
 
-    this.route.params.subscribe((params: Params) => {
+    this.paramsSubscription = this.route.params.subscribe((params: Params) => {
       this.userId.id = params['id']
       this.userId.name = params['name']
     })
+  }
+  ngOnDestroy(): void {
+    this.paramsSubscription.unsubscribe()
   }
 
 }
