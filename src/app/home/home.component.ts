c3cfe816d322c3651144f94e64148f92eee91b17
Harish Bompally <harishb@zenwork.com>
2024-06-17 16:00:59 +0530

    observable changes.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObsSubscription: Subscription
+  private subscription2: Subscription
+  ngOnInit(): void {
+  //   this.firstObsSubscription = interval(1000).subscribe(count => {
+  //     console.log(count)
+  //   })
+
+  //   const customObservable = Observable.create(observer => {
+  //     let count = 0
+  //     setInterval(()=>{
+  //       observer.next(count)
+  //       if(count >3){
+  //         observer.error('Greater than 3')
+  //       }
+  //       if(count ===5){
+  //         observer.complete()
+  //       }
+  //       count++
+  //     },1000)
+      
+  //   })
+  //  this.subscription2= customObservable.subscribe(data=>{
+  //     console.log('second observable ', data)
+  //   },
+  //   error =>{
+  //     console.log(error.message)
+  //   },
+  //   ()=>{
+  //     console.log('completed.')
+  //   }
+  // )
+  }
+  ngOnDestroy(): void {
+    // this.firstObsSubscription.unsubscribe()
+    // this.subscription2.unsubscribe()
+  }
   constructor(private router: Router, private authservice: AuthService) { }
   loadServer() {
     this.router.navigate(['/servers'], { queryParams: { per_page: 1 } })
